3e6732a7d20792cf7e8c769351bbe104ebb35fee
Mojtaba Rahimy <mojtaba.rahimy@chmail.ir>
2020-04-14 10:39:34 +0430

    adding more ui, using db in backend

--- toss_api.php
+++ toss_api.php
@@ -1,6 +1,18 @@
 <?php
 header('Access-Control-Allow-Origin: *'); 
 
+$servername = "localhost";
+$username = "cafesqir_root";
+$password = "iHNqXYexdFBg";
+$db = "cafesqir_spy";
+
+$conn = new mysqli($servername, $username, $password, $db);
+$conn-> set_charset("utf8");
+
+if ($conn->connect_error) {
+	die("Connection failed: " . $conn->connect_error);
+}
+
 if(isset($_GET['player_count'])){
     $player_count =  $_GET['player_count'];
     if($player_count>=4){
@@ -8,16 +20,23 @@ if(isset($_GET['player_count'])){
         $words_json = json_decode($words_string, true);
         $randomIndex = array_rand($words_json);
         $randomWord = $words_json[$randomIndex];
-        $_SESSION['random_word'] = $randomWord;
-        $_SESSION['player_count'] = $player_count;
+        $gameCode = gen_uid($l=10);
+        while(isGameCodeTaken($gameCode, $conn)){
+            $gameCode = gen_uid($l=10);
+        }
+        $sql = "INSERT INTO game (code, nightname, player_count) VALUES ('$gameCode','$randomWord','$player_count');";
+	    runQuery($sql, $conn);
         $indices = range(1, $player_count);
         shuffle($indices);
-        $_SESSION['role_'.$indices[0]] = 'spy';
+        $sql = "INSERT INTO player (game, position, is_assigned, is_spy) VALUES ('$gameCode',$indices[0],false, true);";
+        runQuery($sql , $conn);
         unset($indices[0]);
-        foreach ($indices as $index) {
-            $_SESSION['role_'.$index] = $randomWord;
+        foreach($indices as $index){
+            $sql = "INSERT INTO player (game, position, is_assigned, is_spy) VALUES ('$gameCode',$index,false, false);";
+            runQuery($sql , $conn);
         }
-        echo $_SESSION['role_1'];
+        
+        showRole($gameCode, 1, $conn);
     }
 }
 
@@ -28,9 +47,45 @@ if(isset($_GET['toss_roles_index'])){
     echo $_SESSION['role_'.$index];
 }
 
-//echo "<p>$randomWord</p>";
+function gen_uid($l=10){
+    return substr(str_shuffle("0123456789abcdefghijklmnopqrstuvwxyz"), 0, $l);
+}
 
-// echo $_SESSION['randomWord']." ".$_SESSION['player_count'];
-// print_r($_SESSION);
+function isGameCodeTaken($code, $conn){
+    if($result = $conn->query("SELECT * FROM game where code=`$code`"))
+	        return $result->num_rows >0;
+}
+
+function runQuery($sql , $conn){
+    if ($conn->query($sql) === TRUE) {
+// 		echo "New record created successfully";
+	} else {
+	   // echo "Error: " . $sql . "<br>" . $conn->error;
+    }
+}
+
+function showRole($gameCode, $position, $conn){
+    if($result = $conn->query("SELECT player.is_spy, game.nightname FROM game JOIN player ON player.game=game.code WHERE player.game='$gameCode' AND player.position=$position LIMIT 1"))
+        if($result->num_rows >0){
+            $row = $result->fetch_assoc();
+            if($row["is_spy"]) echo 'spy';
+            else echo $row["nightname"];
+        }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 ?>
